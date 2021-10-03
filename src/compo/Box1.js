ca6d1b7962929d453391404122c043431d5b3a84
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-10-03 15:24:06 -0500

    added-useState

--- src/compo/Box1.js
+++ src/compo/Box1.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React,{useState} from 'react';
+import {motion} from 'framer-motion';
+
 const Box1 = () => {
+    const [isanimated,setIsAnimated] = useState(false)
+
   return (
     <div className="box-container">
     <motion.div 
@@ -8,19 +11,28 @@ const Box1 = () => {
     animate ={{
         x:1500,
        opacity:1,
+       backgroundColor:'red',
+    //    scale:2,
+     rotate:360
+      
     }}
     initial={{
         opacity:0.01,
+        
+       
     }}
     transition={{
         // type:'tween',
         // duration:3
         type:'spring',
-        stiffness:300
+        stiffness:60,
+        damping:7
+        //if you dawn damping moveUp
     }}
+    onClick ={() =>setIsAnimated(!isanimated)}
 
     >
-        <h3>Box-1</h3>
+        <h1>Box-1</h1>
     </motion.div>
     </div>
   )
